eb3d3e6890f7e668cd5dbfd6ffb34ced5b8777dd
Ali Mohamed <ali.elsabbagh@ejust.edu.eg>
2026-01-21 22:29:35 +0200

    1. created AppUser 2. created AbbDbContext with propertise configurations

---
+++ src/Inventory.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Inventory.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Inventory.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfraStructure(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<AppDbContext>(options => 
+            {
+                options.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+        }
+    }
+}
