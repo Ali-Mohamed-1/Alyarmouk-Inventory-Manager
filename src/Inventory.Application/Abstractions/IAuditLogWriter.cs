cd9fc1a4bf5c17d93cd7bc0d2a947fcbf213da37
Ali Mohamed <ali.elsabbagh@ejust.edu.eg>
2026-01-22 12:57:04 +0200

    Implemented IAuditLogWriter interface

--- src/Inventory.Application/Abstractions/IAuditLogWriter.cs
+++ src/Inventory.Application/Abstractions/IAuditLogWriter.cs
@@ -8,7 +8,7 @@ namespace Inventory.Application.Abstractions
     public interface IAuditLogWriter
     {
         /// <summary>
-        /// Records a Create action for an entity with an integer ID
+        /// Records a Create action for an entity
         /// </summary>
         Task LogCreateAsync<T>(object entityId, UserContext user, object? afterState = null, CancellationToken ct = default) where T : class;
 
@@ -18,7 +18,7 @@ namespace Inventory.Application.Abstractions
         Task LogUpdateAsync<T>(object entityId, UserContext user, object? beforeState = null, object? afterState = null, CancellationToken ct = default) where T : class;
 
         /// <summary>
-        /// Records a Delete action for an entity with a long ID
+        /// Records a Delete action for an entity
         /// </summary>
         Task LogDeleteAsync<T>(object entityId, UserContext user, object? beforeState = null, CancellationToken ct = default) where T : class;
 
